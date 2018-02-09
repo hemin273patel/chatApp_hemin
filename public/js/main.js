2aab56b588ce253368303ed91e0d946c148b20ff
Patel <h_patel273@tss-hr420-dm19.fc.ca>
2018-02-09 10:26:01 -0500

    Pushhing the second class file

---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+(()=> {
+    const socket = io();
+    let messageList = document.querySelector('ul'),
+        chatForm = document.querySelector('form'),
+        nameInput = document.querySelector('.nickname'),
+        chatMessage = chatForm.querySelector('.message'),
+        nickName = null;
+    
+    function setNickname(){
+        nickName = this.value;
+    }
+    
+    function handleSendMessage(e){
+        e.preventDefault();// block default event(refresh page)
+        nickName = (nickName && nickName.length > 0) ? nickName : 'user';
+        msg = `${nickName} says ${chatMessage.value}`;
+        
+        socket.emit('chat message', msg);
+        chatMessage.value= "";
+        return false;
+    }
+    
+    
+    function appendMessage(msg) {
+        
+        //debugger;
+        let newMsg = `<li>${msg.message}</li>`;
+        messageList.innerHTML += newMsg;
+    }
+    
+    function appendDiscMessage(msg){
+        //debugger;
+        let newMsg = `<li>${msg}</li>`;
+        messageList.innerHTML += newMsg;
+    }
+    
+
+    
+    nameInput.addEventListener('change', setNickname, false);
+    chatForm.addEventListener('submit', handleSendMessage, false);
+    socket.addEventListener('chat message', appendMessage, false);
+    socket.addEventListener('disconnect message', appendDiscMessage, false);
+    
+})();
